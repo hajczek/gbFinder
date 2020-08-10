3d716686dd145af030a2a588e8cd46b2410b6276
hajczek <i.hajczewska@gmail.com>
2020-08-10 10:38:49 +0200

    Added option for get users who were search to searchUsers function, comment general function for display all users from api

--- src/App.js
+++ src/App.js
@@ -11,18 +11,23 @@ class App extends Component {
     loading: false,
   };
 
-  async componentDidMount() {
-    this.setState({ loading: true });
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
 
+  //   const res = await axios.get(
+  //     `https:/api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search Github Users
+  searchUsers = async (text) => {
     const res = await axios.get(
-      `https:/api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https:/api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ users: res.data, loading: false });
-  }
-
-  searchUsers = (text) => {
-    console.log(text);
+    this.setState({ users: res.data.items, loading: false });
   };
 
   render() {
