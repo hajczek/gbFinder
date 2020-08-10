306525c254a99cf3a6afa14b7f98a2303c4f15b3
hajczek <i.hajczewska@gmail.com>
2020-08-10 15:31:43 +0200

    Added function for taking user login from path

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 
 export class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+  }
   render() {
     return <div>User</div>;
   }
