a5653ac85d115b508fdfc21d68afc713a3fc5929
hajczek <i.hajczewska@gmail.com>
2020-08-10 16:43:47 +0200

    Added function getUserRepos and another state for repos

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends Component {
     user: {},
     loading: false,
     alert: null,
+    repos: [],
   };
 
   // For display all users from api without search
@@ -46,6 +47,16 @@ class App extends Component {
     );
 
     this.setState({ user: res.data, loading: false });
+  };
+
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https:/api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
   };
 
   // Clear users from state
