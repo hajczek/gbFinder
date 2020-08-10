045f2eb3f389405ce4ce2d23dac6210a02e9a7b2
hajczek <i.hajczewska@gmail.com>
2020-08-10 11:19:51 +0200

    Set prop types for setAlert prop

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,6 +10,7 @@ export class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onSubmit = (e) => {
