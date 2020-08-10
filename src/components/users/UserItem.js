d5db200a9ab67c8dc8a6a97d19994ce84b36c3b4
hajczek <i.hajczewska@gmail.com>
2020-08-10 15:28:44 +0200

    Changed a element to Link

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
-    <div className='card text-center'>
+    <div className="card text-center">
       <img
         src={avatar_url}
-        alt=''
-        className='round-img'
-        style={{ width: '60px' }}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
       />
       <h3>{login}</h3>
       <div>
-        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+        <Link to={`/user/${login}`} className="btn btn-dark btn-sm my-1">
           More
-        </a>
+        </Link>
       </div>
     </div>
   );
