9dfa25327d503bbfba067a98f68cf70940ab3903
hajczek <i.hajczewska@gmail.com>
2020-08-10 16:28:29 +0200

    Added info about user login, company and website

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -22,6 +22,7 @@ export class User extends Component {
       location,
       bio,
       blog,
+      company,
       website,
       login,
       html_url,
@@ -68,6 +69,29 @@ export class User extends Component {
             <a href={html_url} className="btn btn-dark my-1">
               Visit Github Profile
             </a>
+            <ul>
+              <li>
+                {login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )}
+              </li>
+              <li>
+                {blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )}
+              </li>
+            </ul>
           </div>
         </div>
       </Fragment>
