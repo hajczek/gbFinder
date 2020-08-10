268bb509994b0c898de5fcdde99036c6961535e8
hajczek <i.hajczewska@gmail.com>
2020-08-10 11:06:55 +0200

    Use destructuring for this.props for cleaner code

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -21,6 +21,7 @@ export class Search extends Component {
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -37,11 +38,8 @@ export class Search extends Component {
             className="btn btn-dark btn-block"
           />
         </form>
-        {this.props.showClear && (
-          <button
-            className="btn btn-light btn-block"
-            onClick={this.props.clearUsers}
-          >
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
             Clear
           </button>
         )}
