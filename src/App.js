05569a43f26b4f6f5eaf5af608bc0147e0b9fb5d
hajczek <i.hajczewska@gmail.com>
2020-08-10 11:02:14 +0200

    Added condition for display clear button only when some users are displays

--- src/App.js
+++ src/App.js
@@ -40,7 +40,11 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
