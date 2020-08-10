1b899d47f5498b46900fdd370da3bf18877e4fce
hajczek <i.hajczewska@gmail.com>
2020-08-10 16:35:59 +0200

    Added more information about user, like following, public repos and public gists

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -96,6 +96,9 @@ export class User extends Component {
         </div>
         <div className="card text-center">
           <div className="badge badge-primary">Followers: {followers}</div>
+          <div className="badge badge-success">Following: {following}</div>
+          <div className="badge badge-light">Public Repos: {public_repos}</div>
+          <div className="badge badge-dark">Public Gists: {public_gists}</div>
         </div>
       </Fragment>
     );
