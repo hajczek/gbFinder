06b156abec798f094bfc11d973cfd6588a0c5a97
hajczek <i.hajczewska@gmail.com>
2020-08-10 11:37:22 +0200

    Added setTimeout to setAlert function for clear displayed alert after 5 seconds

--- src/App.js
+++ src/App.js
@@ -40,6 +40,8 @@ class App extends Component {
   // Set Alert
   setAlert = (msg, type) => {
     this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
   };
 
   render() {
