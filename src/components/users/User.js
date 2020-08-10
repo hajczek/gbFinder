491f63569ee87ee2c43e61d5eeae30de6e569c3f
hajczek <i.hajczewska@gmail.com>
2020-08-10 16:17:22 +0200

    Added code for display card with user image

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -17,7 +17,7 @@ export class User extends Component {
   render() {
     const {
       name,
-      avatar,
+      avatar_url,
       url,
       location,
       bio,
@@ -47,6 +47,16 @@ export class User extends Component {
         ) : (
           <i className="fas fa-times-circle text-danger" />
         )}
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              alt=""
+              className="round-img"
+              style={{ width: "150px" }}
+            />
+          </div>
+        </div>
       </Fragment>
     );
   }
