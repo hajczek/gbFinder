fa25a487042c93714c018e2405c4acc73ccf0b5d
hajczek <i.hajczewska@gmail.com>
2020-08-11 13:54:51 +0200

    Imported GithucContext and remove user, loading and getUser from props

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,12 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useContext } from "react";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import GithubContext from "../../context/github/githubContext";
 
-const User = ({ user, loading, getUser, getUserRepos, repos, match }) => {
+const User = ({ getUserRepos, repos, match }) => {
+  const githubContext = useContext(GithubContext);
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
