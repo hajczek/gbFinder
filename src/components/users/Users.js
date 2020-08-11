16be3e57e380919995f86545d2bcef72fdff1105
hajczek <i.hajczewska@gmail.com>
2020-08-11 13:13:30 +0200

    Brought, implemented and use GithubContext

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import UserItem from './UserItem';
-import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import React, { useContext } from "react";
+import UserItem from "./UserItem";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import GithubContext from "../../context/github/githubContext";
 
-const Users = ({ users, loading }) => {
+const Users = () => {
+  const githubContext = useContext(GithubContext);
+
+  const { loading, users } = githubContext;
+
   if (loading) {
     return <Spinner />;
   } else {
@@ -23,9 +28,9 @@ Users.propTypes = {
 };
 
 const userStyle = {
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)',
-  gridGap: '1rem',
+  display: "grid",
+  gridTemplateColumns: "repeat(3, 1fr)",
+  gridGap: "1rem",
 };
 
 export default Users;
