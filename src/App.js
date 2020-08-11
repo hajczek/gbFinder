4c5abcfce81cabf78953beecafcbf721946c18a2
hajczek <i.hajczewska@gmail.com>
2020-08-11 16:58:10 +0200

    Imported and added route for component NotFound

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import Alert from "./components/layout/Alert";
 import Home from "./components/pages/Home";
 import User from "./components/users/User";
 import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
+
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
+
 import "./App.css";
 
 const App = () => {
@@ -22,6 +25,7 @@ const App = () => {
                 <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route exact path="/user/:login" component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
