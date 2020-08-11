cc6319998d33a787a74cd17f16a2fd1e9921a132
hajczek <i.hajczewska@gmail.com>
2020-08-11 14:00:14 +0200

    Remove not needed elements from propTypes

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -103,10 +103,7 @@ const User = ({ getUserRepos, repos, match }) => {
 };
 
 User.propTypes = {
-  loading: PropTypes.bool.isRequired,
-  user: PropTypes.object.isRequired,
   repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
   getUserRepos: PropTypes.func.isRequired,
 };
 
